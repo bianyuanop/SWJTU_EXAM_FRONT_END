46cd0c4033bdf6dfd929b71b4f68f63f5ac43727
bianyuanop <m13320507359@163.com>
2021-05-01 12:41:28 +0800

    something

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class="home">
+  <div class="home" @click="interact">
     This is sweet home.
   </div>
 </template>
 
 <script>
+import $ from 'jquery'
 export default {
   name: 'Home',
   components: {
+  },
+  data() {
+    return {
+      data: "something"
+    }
+  },
+  methods: {
+    interact: function() {
+      $.post({
+        url: 'exam.shinenet.cn/api/auth/register',
+        data: {username: "chan", password: "test", student_id: 2019112256},
+        dataType: 'json',
+        success: function(data) {
+          console.log(data);
+        }
+      })
+    }
   }
 }
 </script>
